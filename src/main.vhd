134bc87e8c82e60f4bda37c8e5df0ec8e573125f
Asagegege <SiroKuro9191@gmil.com>
2026-07-11 09:30:31 +0900

    first commit

---
+++ src/main.vhd
@@ -0,0 +1,20 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+use IEEE.numeric_std.all;
+
+entity main is
+    port(
+        RST : in std_logic;
+        CLK : in std_logic;
+
+        --to VGAport
+        ----from DAC
+        ----from VGA-ctrl
+
+    );
+end main;
+
+architecture RTL of main is
+    begin
+end RTL;
